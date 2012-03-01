e9d6031d71172cc62c3923f63c464c00be0c7ae9
tatthang <tatthang@IBM-H7RFJDJTC8Q>
2012-03-01 23:21:37 +0800

    Initial Commit

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import GUI.LoginForm;
+/**
+ *
+ * @author tatthang
+ */
+public class main {
+    public static void main(String[] args){
+        LoginForm frm = new LoginForm();
+        frm.show();
+    }
+}
